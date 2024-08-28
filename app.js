7d1ba479ff6238839afd7a8a55f7672fbb0136ef
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-28 19:32:48 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 
+
+
+app.use((req, res, next)=> {
+    console.log('hii uttam');
+    next();
+})
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
-
+app.get('/profile', (req, res) => {
+    res.send('Hello profile!');
+    // return next(new Error("something went wrong"));
+})
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(500).send("something broke !");
+})
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
 });
