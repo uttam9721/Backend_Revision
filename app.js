172af49e994d173b6da18ba2b97e4c4082e7d002
Uttam Kumar <uttammaurya377@gmail.com>
2024-08-29 00:26:01 +0530

    done

--- app.js
+++ app.js
@@ -57,3 +57,38 @@
 // app.listen(3000,()=>{
     // console.log("port on running 3000")
 // })
+
+const express = require('express');
+const app = express();
+const path = require('path'); 
+const fs=require('fs'); 
+
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',function(req,res){
+    // res.send("hello world");
+    fs.readdir(`./files`,(err,files)=>{
+            // console.log(files);
+            res.render("index",{files:files});
+    })
+   
+})
+app.post('/create',(req,res)=>{
+    // res.send("hello world");
+    // console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.detail,function(err){
+        res.redirect("/");
+    })
+   
+   
+})
+
+
+app.listen(3000,()=>{
+    console.log("port on running 3000")
+})
